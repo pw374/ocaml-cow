3a273569c6f092961ab8b3e15c7a7d4bb0e79688
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-01-26 18:58:06 +0000

    Add tests for CSS parser

---
+++ lib_tests/test_css.ml
@@ -0,0 +1,18 @@
+let () =
+  if Array.length Sys.argv <= 1 then (
+    Printf.eprintf "usage: %s <filename.css>" (Filename.basename Sys.argv.(0));
+    exit 1;
+  )
+
+let () =
+  let read1 = Css.of_file Sys.argv.(1) in
+  let str1 = Css.to_string read1 in
+  Printf.printf "CSS1(%s):\n%s\n" Sys.argv.(1) str1;
+  let read2 = Css.of_string str1 in
+  let str2 = Css.to_string read2 in
+  Printf.printf "CSS2(%s):\n%s\n" Sys.argv.(1) str2;
+  Printf.printf "\n\nread1=read2 %b\n%!" (read1 = read2);
+  Css.check read1 read2;
+  assert (read1 = read2)
+
+
