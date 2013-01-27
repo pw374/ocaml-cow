51417ed64895d726e027b29363bb9e24434789c3
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-01-27 00:55:06 +0000

    Import markdown unit-tests from https://github.com/coreyti/showdown

--- lib_tests/css/test.ml
+++ lib_tests/css/test.ml
@@ -8,11 +8,11 @@ let () =
   let read1 = Css.of_file Sys.argv.(1) in
   let str1 = Css.to_string read1 in
   Printf.printf "CSS1(%s):\n%s\n" Sys.argv.(1) str1;
+
   let read2 = Css.of_string str1 in
   let str2 = Css.to_string read2 in
   Printf.printf "CSS2(%s):\n%s\n" Sys.argv.(1) str2;
-  Printf.printf "\n\nread1=read2 %b\n%!" (read1 = read2);
-  Css.check read1 read2;
-  assert (read1 = read2)
+
+  Css.assert_equal read1 read2
 
 
